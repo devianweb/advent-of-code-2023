53c60eafb298eea2d3955c7d0a9598c8fdd15a03
carrcig <87659522+carrcig@users.noreply.github.com>
2023-12-02 21:33:50 +0000

    day 1 solved

---
+++ day_1.py
@@ -0,0 +1,55 @@
+import re
+
+def convert_word_to_number(word):
+    match word:
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case "seven":
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+        case _:
+            return word
+        
+def find_numbers(value):
+    matches = re.finditer("(?=(\\d|one|two|three|four|five|six|seven|eight|nine))", value)
+    return [str(match.group(1)) for match in matches]
+        
+        
+def calibrtion_decoder(value):
+    numbers = find_numbers(value)
+    
+    print("numbers: ")
+    print(numbers)
+
+    first_number = convert_word_to_number(numbers[0])
+    last_number = convert_word_to_number(numbers[-1])
+
+    print("first_number: " + first_number)
+    print("last_number: " + last_number)
+
+    combined_number = first_number + last_number
+
+    print(combined_number)
+    return combined_number
+
+total = 0
+
+with open('day_1_input.txt', 'r') as file:
+    for line in file:
+        result = calibrtion_decoder(line)
+        total += int(result)
+
+print("total: " + str(total))
